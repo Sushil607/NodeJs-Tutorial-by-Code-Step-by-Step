9b013742ed8ae20e28bf4952fa7cb97c4dbe4077
Sushil607 <bodadesushil@gmail.com>
2023-01-26 20:34:08 +0530

    search api and file upload using multer

--- mongoose-apis.js
+++ mongoose-apis.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { response } = require("express/lib/express");
+const multer = require("multer");
 // Configuration, already executed if imported
 require("./config");
 const Product = require("./product");
@@ -29,6 +29,32 @@ app.put("/products/:id", async (req, res) => {
   const updatedProduct = req.body;
   let data = await Product.updateOne({ _id: id }, { $set: updatedProduct });
   res.send(data);
+});
+
+app.get("/search/:key", async (req, res) => {
+  let data = await Product.find({
+    $or: [
+      { name: { $regex: req.params.key } },
+      { brand: { $regex: req.params.key } },
+      { category: { $regex: req.params.key } },
+    ],
+  });
+  res.send(data);
+});
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + "-" + Date.now() + ".jpg");
+    },
+  }),
+}).single("user_file");
+
+app.post("/upload", upload, (req, res) => {
+  res.send("uploaded...");
 });
 
 app.listen(3000, () => {
